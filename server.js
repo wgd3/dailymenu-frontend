fa1545aec36509bddb14954f352a78dce4bedf5e
wgd3 <>
2021-01-18 16:50:06 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
+//Install express server
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static("./dist/dailymenu-frontend"));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/dist/dailymenu-frontend"));
 
-app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/dailymenu-frontend/" })
-);
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/dailymenu-frontend/index.html"));
+});
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
